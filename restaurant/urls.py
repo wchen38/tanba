2e4ac9d497b2664d69ad62c4821c74c8dfe719e8
WUYUAN CHEN <wuyuanchen123@gmail.com>
2021-06-03 21:54:57 -0700

    able to display the menu item name to view

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,5 +4,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.menu_detail, name='menu_detail'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from .models import Menu
+
 def index(request):
     return HttpResponse("Hello World! This is your restaurant home page")
+
+def menu_detail(request):
+    categories = Menu.objects.all()
+    output = ""
+    for category in categories:
+        import pdb; pdb.set_trace()
+        items = category.menuitem_set.all()
+        output += ', '.join([item.name for item in items])
+    return HttpResponse("you are looking at the menu: {}".format(output))
