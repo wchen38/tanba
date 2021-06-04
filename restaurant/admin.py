72690b01bb85ce6f4f82af32c5891880836f7e13
WUYUAN CHEN <wuyuanchen123@gmail.com>
2021-06-03 20:52:02 -0700

    modified the admin page to display menu items and order items

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Store, Menu, Order
+from .models import Store, Menu, MenuItem, Order, OrderItem
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    extra = 3
+
+
+class MenuAdmin(admin.ModelAdmin):
+    inlines = [MenuItemInline]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 3
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Personal Infomation', {'fields': ['first_name', 'last_name', 'email', 'phone_number'], 'classes': ['collapse']})
+    ]
+    inlines = [OrderItemInline]
+
 
 admin.site.register(Store)
-admin.site.register(Menu)
-admin.site.register(Order)
+admin.site.register(Menu, MenuAdmin)
+admin.site.register(Order, OrderAdmin)
